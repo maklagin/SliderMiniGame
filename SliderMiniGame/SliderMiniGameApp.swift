585f3b9d43c6bca087e26b6bffef904e15b47ffa
maklagin <108239343+maklagin@users.noreply.github.com>
2022-09-20 13:47:14 +0300

    Initial Commit

---
+++ SliderMiniGame/SliderMiniGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderMiniGameApp.swift
+//  SliderMiniGame
+//
+//  Created by Максим Кулагин on 20.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct SliderMiniGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
